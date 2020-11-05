f05c71048558ab6081078ce5ed511c5a3431c849
Andrew Talle <talle.andrew@gmail.com>
2020-11-05 13:21:32 -0800

    show transaction items

---
+++ script.js
@@ -0,0 +1,40 @@
+// balance money_plus money_minus list form text amount
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('form');
+
+// dummy vars
+const transactions = [
+  { id: 1, text: 'PS5', amount: -450 },
+  { id: 2, text: 'Salary', amount: 1000 },
+  { id: 3, text: 'Airline', amount: -300 },
+  { id: 4, text: 'PhotoShoot', amount: -100}
+];
+
+function addTransactionDOM(transaction) {
+  // Get Sign
+  const sign = transaction.amount < 0 ? '-' : '+';
+  const item = document.createElement('li');
+
+  // Add class based on value
+  item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+
+  item.innerHTML = `
+    ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span><button class="delete-btn">x</button>
+  `;
+
+  list.appendChild(item);
+}
+
+
+// Init app
+function init() {
+  list.innerHTML = '';
+  transactions.forEach(addTransactionDOM)
+}
+
+init();
